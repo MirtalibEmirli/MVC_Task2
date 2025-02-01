8653f6122ab1eedf6ac7aaea0d6d537cd51820f0
Amirl_kl34@itstep.edu.az <Amirl_kl34@itstep.edu.az>
2025-02-02 00:01:48 +0400

    razorpage editt and delete

---
+++ MVC_Task2/Pages/Edit.cshtml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MVC_Task2.Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+namespace MVC_Task2.Pages
+{
+    public class EditModel : PageModel
+    {
+        [BindProperty]
+        public User User { get; set; } = new User();   
+        public List<User> Users { get; set; } = new List<User>();
+
+        public void OnGet(Guid id)
+        {
+           
+
+            Users = ReadAllUsers();
+            User = Users.FirstOrDefault(x => x.Id == id);
+
+          
+        }
+
+        public IActionResult OnPost()
+        {
+            Users = ReadAllUsers();
+            var existingUser = Users.FirstOrDefault(u => u.Id == User.Id);
+
+            if (existingUser != null)
+            {
+          
+                existingUser.FirstName = User.FirstName;
+                existingUser.LastName = User.LastName;
+                existingUser.Description = User.Description;
+                existingUser.Img = User.Img;
+            }
+            else
+            { 
+                Users.Add(User);
+            }
+
+            WriteAllUsers();  
+            return RedirectToPage("Page1");
+        }
+
+
+
+        public List<User> ReadAllUsers()
+        {
+            string path = @"Helpers\users.json";
+
+            if (!System.IO.File.Exists(path))
+            {
+                return new List<User>();
+            }
+
+            string text = System.IO.File.ReadAllText(path);
+            return JsonSerializer.Deserialize<List<User>>(text) ?? new List<User>();
+        }
+
+        public void WriteAllUsers()
+        {
+            string path = @"Helpers\users.json";
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            var data = JsonSerializer.Serialize(Users, options);
+            System.IO.File.WriteAllText(path, data);
+        }
+    }
+}
--- MVC_Task2/Pages/Page1.cshtml.cs
+++ MVC_Task2/Pages/Page1.cshtml.cs
@@ -5,6 +5,7 @@ using MVC_Task2.Entities.Concretes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 
 namespace MVC_Task2.Pages
 {
@@ -12,7 +13,7 @@ namespace MVC_Task2.Pages
     {
         public Page1Model(FreelancerDbcontext freelancerDbcontext)
         {
-                dbcontext = freelancerDbcontext;
+            dbcontext = freelancerDbcontext;
         }
         private readonly FreelancerDbcontext dbcontext;
         public string Message { get; set; }
@@ -22,18 +23,44 @@ namespace MVC_Task2.Pages
         public User User { get; set; }
         public void OnGet()
         {
-            Users = dbcontext.Users.ToList();
+            Users = ReadAllUsers(); 
             Message = $"Now Date  is {DateTime.Now.Date.Day}/{DateTime.Now.Date.Month}";
             Info = "A";
         }
 
         public IActionResult OnPost()
         {
-            dbcontext.Users.Add(User);  
-            dbcontext.SaveChanges();
-            Message =$"{User.FirstName} added";
+            Users.Add(User);
+          WriteAllUsers();
+            Message = $"{User.FirstName} added";
             return RedirectToPage("Page1");
 
         }
-    }
-}
+        public IActionResult OnPostDelete(Guid id)
+        {
+            Users = ReadAllUsers();
+            var userToDelete = Users.FirstOrDefault(x => x.Id == id);
+            if (userToDelete != null)
+            {
+                Users.Remove(userToDelete);
+                WriteAllUsers();
+            }
+            return RedirectToPage();
+
+        }
+        public List<User> ReadAllUsers()
+        {
+            string path = @"Helpers\users.json";
+            string text = System.IO.File.ReadAllText(path);
+            return JsonSerializer.Deserialize<List<User>>(text) ?? new List<User>();
+        }
+
+        public void WriteAllUsers()
+        {
+            string path = @"Helpers\users.json";
+            var options = new JsonSerializerOptions() { WriteIndented = true };
+            string data = JsonSerializer.Serialize(Users);
+            System.IO.File.WriteAllText(path, data);
+        }
+    }   
+}  
